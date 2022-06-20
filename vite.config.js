fc577281b390f9d359523360b636ddd65804bd45
Israel Philips <65095567+IsraelPhilips@users.noreply.github.com>
2022-06-21 00:30:13 +0100

    Vite Config edit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+}
+
 export default defineConfig({
   plugins: [react()]
 })
